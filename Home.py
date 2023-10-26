1b56498983e58a6be144571a88ffdd6618a6b6e8
James Moore II <147284652+James-Moore1808@users.noreply.github.com>
2023-10-26 19:57:46 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 # Define the scope and credentials file
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-gc = gspread.service_account({{secrets.CREDENTIAL_SHEET}})
+gc = gspread.service_account(filename = r"C:\Users\jmu81\NFL Picks 2023-24\Python\credentials-sheets.json")
 #Opening the spreadsheet
 pickLog = gc.open('NFL Pick Log 2023-24')
 results = pickLog.worksheet("Results")
