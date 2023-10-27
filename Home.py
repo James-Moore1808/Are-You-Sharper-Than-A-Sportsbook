a14b0f96f34fa9015d31130099f190da26f226bd
James Moore II <147284652+James-Moore1808@users.noreply.github.com>
2023-10-26 20:52:01 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,9 +11,8 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 # Define the scope and credentials file
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-toml_dict=toml.loads(str(st.secrets["creds"]))
-json_data = json.dumps(toml_dict)
-gc = gspread.service_account(filename = json_data )
+
+gc = gspread.service_account.Credentials.from_service_account_info(st.secrets["creds"], scopes = scope)
 
 
 
