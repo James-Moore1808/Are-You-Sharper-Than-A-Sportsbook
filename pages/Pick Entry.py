067eaad1d6bd695a904b202da7173389d0913c68
James Moore II <147284652+James-Moore1808@users.noreply.github.com>
2023-10-27 18:27:47 -0400

    Update Pick Entry.py

--- pages/Pick Entry.py
+++ pages/Pick Entry.py
@@ -2,6 +2,24 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 import pandas as pd
 
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
+cred_data = {
+  "type": st.secrets["credentials"]["type"],
+  "project_id":  st.secrets["credentials"]["project_id"],
+  "private_key_id": st.secrets["credentials"]["private_key_id"],
+  "private_key":  st.secrets["credentials"]["private_key"],
+  "client_email":  st.secrets["credentials"]["type"],
+  "client_id":  st.secrets["credentials"]["client_email"],
+  "auth_uri":  st.secrets["credentials"]["auth_uri"],
+  "token_uri":  st.secrets["credentials"]["token_uri"],
+  "auth_provider_x509_cert_url":  st.secrets["credentials"]["auth_provider_x509_cert_url"],
+  "client_x509_cert_url":  st.secrets["credentials"]["client_x509_cert_url"],
+  "universe_domain":  st.secrets["credentials"]["universe_domain"]
+}
+
+# Define the scope and credentials file
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+gc = gspread.service_account_from_dict(st.secrets["credentials"])
 
 st.write("What are yoiu doing in this mf")
