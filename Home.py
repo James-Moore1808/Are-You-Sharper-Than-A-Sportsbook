8da40d2fa9e9ee3813b9d15d1de23ad96d236a5d
James Moore II <147284652+James-Moore1808@users.noreply.github.com>
2023-10-28 09:40:49 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,23 +10,16 @@ st.set_page_config(page_title="Are You Sharper Than a Sportsbook?", page_icon= "
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-cred_data = {
-  "type": st.secrets["credentials"]["type"],
-  "project_id":  st.secrets["credentials"]["project_id"],
-  "private_key_id": st.secrets["credentials"]["private_key_id"],
-  "private_key":  st.secrets["credentials"]["private_key"],
-  "client_email":  st.secrets["credentials"]["type"],
-  "client_id":  st.secrets["credentials"]["client_email"],
-  "auth_uri":  st.secrets["credentials"]["auth_uri"],
-  "token_uri":  st.secrets["credentials"]["token_uri"],
-  "auth_provider_x509_cert_url":  st.secrets["credentials"]["auth_provider_x509_cert_url"],
-  "client_x509_cert_url":  st.secrets["credentials"]["client_x509_cert_url"],
-  "universe_domain":  st.secrets["credentials"]["universe_domain"]
-}
 
 # Define the scope and credentials file
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+#use to deploy
 gc = gspread.service_account_from_dict(st.secrets["credentials"])
+
+#for local
+#gc = gspread.service_account(filename = r"C:\Users\jmu81\NFL Picks 2023-24\Python\credentials-sheets.json")
+
 #Opening the spreadsheet
 pickLog = gc.open("NFL Pick Log 2023-24")
 results = pickLog.worksheet("Results")
