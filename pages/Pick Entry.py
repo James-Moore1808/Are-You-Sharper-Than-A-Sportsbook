1fb7119db82f05cbf3d8974308856b1f2494bf6f
James Moore II <147284652+James-Moore1808@users.noreply.github.com>
2023-10-29 08:04:34 -0400

    Update Pick Entry.py

--- pages/Pick Entry.py
+++ pages/Pick Entry.py
@@ -27,7 +27,13 @@ st.divider()
 
 message = "To enter and/or view picks you must enter a valid Username and Password"
 
-def verification():
+
+
+
+with st.form("Login"):
+    username = st.text_input("Username", placeholder = None)
+    password = st.text_input("Password", placeholder = None , type="password")
+    def verification():
     if username in (accounts['Username']) == True:
         if password == st.secrets["password"][username]:
             st.write(":green[Sucessful Login]")
@@ -38,10 +44,7 @@ def verification():
     else:
         st.write(":red[Please enter a Username and/or a Password]")
 
-
-with st.form("Login"):
-    username = st.text_input("Username", placeholder = None)
-    password = st.text_input("Password", placeholder = None , type="password")
+    
     submit = st.form_submit_button("Submit login information", on_click = verification, args = None)
     
     
