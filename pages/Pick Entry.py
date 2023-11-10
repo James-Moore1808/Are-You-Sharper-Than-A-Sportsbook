0f3fdab66488699133a2e2bf22c4b97151a85ec2
James Moore II <james.moore651808@gmail.com>
2023-11-10 17:56:09 -0500

    x

--- pages/Pick Entry.py
+++ pages/Pick Entry.py
@@ -173,7 +173,7 @@ if st.session_state.account_counter == 2:
                 team_list,
                 captions = [scoreboard_df.query(f"Team=='{team_list[0]}'")['Spread'].to_list()[0],scoreboard_df.query(f"Team=='{team_list[1]}'")['Spread'].to_list()[0]],
             )
-            selected_team = game
+            st.session_state.selected_team = game
             next_button = st.form_submit_button(label="Next", use_container_width=True, on_click=next_clicked)
             back_button = None
     elif st.session_state.counter > 0 and st.session_state.counter < st.session_state.lastrow_picks:
