e51c3777aaaf60e01b67bdaa89f1d687d2cf2086
James Moore II <james.moore651808@gmail.com>
2023-11-10 17:46:53 -0500

    CHANGE

--- pages/Pick Entry.py
+++ pages/Pick Entry.py
@@ -148,15 +148,15 @@ if st.session_state.account_counter == 2:
         st.session_state['spreads'].append(list)
 
     #defining on click function to advance    
-    def next_clicked():
-        selection = st.session_state.selected_team
+    def next_clicked(selected_team):
+        selection = selected_team
         if selection not in team_list:
             st.write(":red[Pick a team before moving to the next selection]")
         else:
             save_picks(selection)
             save_spreads(scoreboard_df.query(f"Team=='{selection}'")['Spread'])
             save_counter()
-            return(st.write("You picked the " + selection))
+            return(st.write("You picked the " + selection), st.session_state.counter)
 
     def back_clicked():
         if team_list[0] in st.session_state.picks or team_list[1] in st.session_state.picks :
@@ -174,7 +174,7 @@ if st.session_state.account_counter == 2:
                 captions = [scoreboard_df.query(f"Team=='{team_list[0]}'")['Spread'].to_list()[0],scoreboard_df.query(f"Team=='{team_list[1]}'")['Spread'].to_list()[0]],
             )
             selected_team = game
-            next_button = st.form_submit_button(label="Next", use_container_width=True)
+            next_button = st.form_submit_button(label="Next", use_container_width=True, on_click=next_clicked, args=selected_team)
             back_button = None
     elif st.session_state.counter > 0 and st.session_state.counter < st.session_state.lastrow_picks:
         team_list = [st.session_state.home_col[st.session_state.counter], st.session_state.away_col[st.session_state.counter]]
