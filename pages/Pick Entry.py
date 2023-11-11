34e02ef33970e416212ddc09745a5998762f7f96
James Moore II <james.moore651808@gmail.com>
2023-11-10 22:28:21 -0500

    mm

--- pages/Pick Entry.py
+++ pages/Pick Entry.py
@@ -156,6 +156,7 @@ if st.session_state.account_counter == 2:
     def next_clicked():
         selection = st.session_state.selected_team
         if selection not in team_list:
+            st.experimental_rerun()
             st.write(":red[Pick a team before moving to the next selection]")
         else:
             save_picks(selection)
